abcefe10013d59d0ad77eee1ff052154c812697f
Your Name <you@example.com>
2023-05-27 00:53:10 +0700

    mengubah menu admin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,150 +1,123 @@
 <!-- Page Content -->
 <div id="page-wrapper">
     <div class="container-fluid">
+        <!-- Content Row -->
         <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Dashboard</h1>
-            </div>
-        </div>
-        <!-- /.row -->
-        <div class="row">
-            <div class="col-lg-3 col-md-6">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-user-o fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9 text-right">
-                                <div>Jumlah Anggota</div>
-                                <div class="huge">?</div>
-                            </div>
+            
+        <!-- Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Anggota</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Kepo</div>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="panel panel-green">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-book fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9 text-right">
-                                <div>Jumlah Berita</div>
-                                <div class="huge">?</div>
-                            </div>
+                        <div class="col-auto">
+                            <i class="fas fa-users fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- /.row -->
-        <div class="row-10">
-            <div class="col-lg-15">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Berita Terbaru
-                    </div>
-                    <!-- /.panel-heading -->
-                    <div class="panel-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Tanggal</th>
-                                        <th>Waktu</th>
-                                        <th>Judul</th>
-                                        <th>Dilihat</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th>---</th>
-                                        <th>---</th>
-                                        <th>---</th>
-                                        <th>---</th>
-                                        <th><a href="#"><button type="button" class="btn btn-primary">Lihat</button></a>
-                                            <a href="#"><button type="button" class="btn btn-warning">Ubah</button></a> <a href="#"><button type="button" class="btn btn-danger">Hapus</button></a>
-                                        </th>
-                                    </tr>
-                                </tbody>
-                            </table>
+        <!-- Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Berita
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Kepo</div>
                         </div>
-                        <!-- /.table-responsive -->
-                    </div>
-                    <!-- /.panel-body -->
-                </div>
-            </div>
-        </div>
-        <!-- /.row -->
-        <div class="row-10">
-            <div class="col-lg-15">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Berita Terpopuler
-                    </div>
-                    <!-- /.panel-heading -->
-                    <div class="panel-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Tanggal</th>
-                                        <th>Waktu</th>
-                                        <th>Judul</th>
-                                        <th>Dilihat</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th>---</th>
-                                        <th>---</th>
-                                        <th>---</th>
-                                        <th>---</th>
-                                        <th><a href="#"><button type="button" class="btn btn-primary">Lihat</button></a>
-                                            <a href="#"><button type="button" class="btn btn-warning">Ubah</button></a> <a href="#"><button type="button" class="btn btn-danger">Hapus</button></a>
-                                        </th>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <!-- /.table-responsive -->
-                    </div>
-                    <!-- /.panel-body -->
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-8">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Kategori Berita
-                    </div>
-                    <!-- /.panel-heading -->
-                    <div class="panel-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Tanggal</th>
-                                        <th>waktu</th>
-                                        <th>Nama Klien</th>
-                                        <th>No Telepon Klien</th>
-                                        <th>Karyawan</th>
-                                        <th> </th>
-                                    </tr>
-                                </thead>
-                            </table>
+                        <div class="col-auto">
+                            <i class="fas fa-book fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-</div>
-
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Berita Terbaru</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                    <th>Tanggal</th>
+                    <th>Waktu</th>
+                    <th>Judul</th>
+                    <th>Dilihat</th>
+                    <th>Aksi</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>-----</td>
+                            <td>-----</td>
+                            <td>-----</td>
+                            <td>-----</td>
+                            <td>
+                                <a href="#" class="btn btn-primary">
+                                    <span class="icon text-white"></span>
+                                    <span class="text">Lihat</span>
+                                </a>
+                                <a href="#" class="btn btn-warning">
+                                    <span class="icon text-white"></span>
+                                    <span class="text">Ubah</span>
+                                </a>
+                                <a href="#" class="btn btn-danger">
+                                    <span class="icon text-white"></span>
+                                    <span class="text">Hapus</span>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Berita Terpopuler</h6>
+    </div>  
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                    <th>Tanggal</th>
+                    <th>Waktu</th>
+                    <th>Judul</th>
+                    <th>Dilihat</th>
+                    <th>Aksi</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>-----</td>
+                            <td>-----</td>
+                            <td>-----</td>
+                            <td>-----</td>
+                            <td>
+                                <a href="#" class="btn btn-primary">
+                                    <span class="icon text-white"></span>
+                                    <span class="text">Lihat</span>
+                                </a>
+                                <a href="#" class="btn btn-warning">
+                                    <span class="icon text-white"></span>
+                                    <span class="text">Ubah</span>
+                                </a>
+                                <a href="#" class="btn btn-danger">
+                                    <span class="icon text-white"></span>
+                                    <span class="text">Hapus</span>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+            </div>
+        </div>
+    </div>
 </div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,14 +1,5 @@
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Portal Berita Desa <?= date('Y'); ?></span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
 
-        </div>
+       </div>
         <!-- End of Content Wrapper -->
 
     </div>
